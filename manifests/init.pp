64b83193dab352eb4205b531f6ccd0f666a3e430
Asher256 <Asher256@users.noreply.github.com>
2016-07-29 19:36:22 -0400

    Boostrap

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+#
+# == Define: lizardfs
+#
+# 'lizardfs::*' classes will help you to configure LizardFS:
+#
+# Requirements:
+# - Puppet >= 3
+# - Operating system: Debian (Wheezy, Jessie), Ubuntu
+#
+# This class follows the recommendations of the "Puppet Labs Style Guide":
+# http://docs.puppetlabs.com/guides/style_guide.html . If you want to
+# contribute, please check your code with puppet-lint.
+#
+# === Authors
+#
+# Copyright (c) Asher256
+# License: Apache 2.0
+#
+# URL: https://github.com/Asher256/puppet-lizardfs
+#
+# === Examples
+#
+# class {'lizardfs':
+# }
+#
+# === Parameters
+#
+# [*foo*]    bar
+#
+class lizardfs()
+{
+}
+
+# vim:et:sw=2:ts=2:sts=2:tw=0:fenc=utf-8
