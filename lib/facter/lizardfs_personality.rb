6a466368e7380c242de13a667d5bdc436c51be6e
Asher256 <Asher256@users.noreply.github.com>
2016-08-04 11:51:55 -0400

    New feature: support for the LizardFS master's first_personality

---
+++ lib/facter/lizardfs_personality.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+#
+# Author: Asher256 <asher256@gameloft.com>
+# License: Apache 2.0
+#
+
+require 'facter'
+
+Facter.add('lizardfs_personality') do
+  begin
+    content = File.open('/etc/lizardfs/.mfsmaster_personality.cfg', 'r') {|fd| fd.readline.chomp}
+  rescue
+    content = nil
+  end
+
+  if content != nil
+    if ['MASTER', 'SHADOW'].include? content
+      setcode { content }
+    end
+  end
+end
+
+# vim:ai:et:sw=2:ts=2:sts=2:tw=78:fenc=utf-8
