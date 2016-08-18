864d4e30f298c788a944799d013e4142585ec1d4
Asher256 <Asher256@users.noreply.github.com>
2016-08-18 12:20:28 -0400

    Pacemaker removed from master.pp + tidy pacemaker.pp

--- manifests/ha/pacemaker.pp
+++ manifests/ha/pacemaker.pp
@@ -1,7 +1,8 @@
 #
 # == Define: lizardfs::ha::pacemaker
 #
-# 'lizardfs::ha::pacemaker': install and configure corosync and pacemaker for HA LizardFS.
+# 'lizardfs::ha::pacemaker': install and configure corosync and pacemaker for
+# HA LizardFS.
 #
 # This class follows the recommendations of the "Puppet Labs Style Guide":
 # http://docs.puppetlabs.com/guides/style_guide.html . If you want to
@@ -18,20 +19,20 @@
 # === Examples
 #
 # class {'lizardfs::ha::pacemaker':
-#   multicast_address          => "239.1.1.2",
-#   pacemaker_quorum_members   => [ 'master.dev', 'shadow.dev' ],
+#   multicast_address  => "239.1.1.2",
+#   quorum_members     => [ 'master.dev', 'shadow.dev' ],
 # }
 #
 # === Parameters
 #
-# [*pacemaker_quorum_members*] a list of the Lizardfs masters IPs or FQDNs (must be resolvable by dns).
-# Ex: pacemaker_quorum_members   => ['lizardfs-master.dev', 'lizardfs-shadow.dev'],
+# [*quorum_members*] a list of the Lizardfs masters IPs or FQDNs (must be resolvable by dns).
+# Ex: quorum_members   => ['lizardfs-master.dev', 'lizardfs-shadow.dev'],
 #
 # [*multicast_address*] the multicast address for corosync service, if you have multiple corosync clusters.
 # Default: multicast_address  = "239.1.1.2",
 #
-# === Important  
-# 
+# === Important
+#
 # Require the module https://forge.puppet.com/puppet/corosync
 #
 # Open in firewall the following ports, besides the ones for lizardfs.
@@ -41,64 +42,64 @@
 # -A INPUT -m tcp -p tcp -m state --state NEW --dport 2224 -j ACCEPT
 #
 ### and the VIP for lfs master service like: options => { MASTER_HOST => "192.168.1.xxx", }
-          
+
 class lizardfs::ha::pacemaker(
-   $multicast_address          = "239.1.1.2",
-   $pacemaker_quorum_members   = [],
+  $multicast_address = "239.1.1.2",
+  $quorum_members    = [],
 )
 {
   if $::osfamily == 'RedHat' {
     yumrepo { "centos-base":
-        mirrorlist => "http://mirrorlist.centos.org/?release=$operatingsystemmajrelease&arch=\$basearch&repo=os",
-        descr => "CentOS-$operatingsystemmajrelease - Base",
-        enabled => 1,
-        gpgcheck => 0,
-        before => Class['corosync'],
+      mirrorlist => "http://mirrorlist.centos.org/?release=$operatingsystemmajrelease&arch=\$basearch&repo=os",
+      descr => "CentOS-$operatingsystemmajrelease - Base",
+      enabled => 1,
+      gpgcheck => 0,
+      before => Class['corosync'],
     }
   }
 
   if ! is_ip_address( $multicast_address ) {
-      fail('The multicast_address is not a valid IP address.')
+    fail('The multicast_address is not a valid IP address.')
   }
 
   if ! is_ip_address( $lizardfs::master::options[MASTER_HOST] ) {
-      fail('Set the virtual IP for HA lfs master service like: options => { MASTER_HOST => "192.168.1.xxx", }.')
+    fail('Set the virtual IP for HA lfs master service like: options => { MASTER_HOST => "192.168.1.xxx", }.')
   }
 
   if ! $lizardfs::master::options[ADMIN_PASSWORD]  {
-      fail('Set the admin password for lizardfs::master service like: options => { ADMIN_PASSWORD => "password", }.')
+    fail('Set the admin password for lizardfs::master service like: options => { ADMIN_PASSWORD => "password", }.')
   }
 
-  if empty($pacemaker_quorum_members) {
-      fail('The pacemaker_quorum_members parameter is required.')
+  if empty($quorum_members) {
+    fail('The quorum_members parameter is required.')
   }
-        
-    class { 'corosync':
-      enable_secauth             => true,
-      authkey                    => "/var/lib/puppet/ssl/certs/ca.pem",
-      bind_address               => $ipaddress,
-      multicast_address          => $multicast_address,
-      manage_pcsd_service        => true,
-      #debug                     => true,
-      set_votequorum             => true,
-      quorum_members             => $pacemaker_quorum_members,
-      #votequorum_expected_votes  => 2,
-      require                    => Class['lizardfs::master'],
-    }
 
-    corosync::service { 'pacemaker':
-      version => '0',
-    }
+  class { 'corosync':
+    enable_secauth             => true,
+    authkey                    => "/var/lib/puppet/ssl/certs/ca.pem",
+    bind_address               => $ipaddress,
+    multicast_address          => $multicast_address,
+    manage_pcsd_service        => true,
+    #debug                     => true,
+    set_votequorum             => true,
+    quorum_members             => $quorum_members,
+    #votequorum_expected_votes  => 2,
+    require                    => Class['lizardfs::master'],
+  }
 
-    cs_property { 'stonith-enabled' :
-      value   => 'false',
-      cib     => 'puppet'
-    }~> Cs_commit['puppet']
+  corosync::service { 'pacemaker':
+    version => '0',
+  }
+
+  cs_property { 'stonith-enabled' :
+    value   => 'false',
+    cib     => 'puppet'
+    } ~> Cs_commit['puppet']
 
     cs_property { 'no-quorum-policy' :
-    value   => 'ignore',
-    cib     => 'puppet'
-    }~> Cs_commit['puppet']
+      value   => 'ignore',
+      cib     => 'puppet'
+    } ~> Cs_commit['puppet']
 
     cs_primitive { 'lizardfs-master':
       primitive_class => 'ocf',
@@ -116,7 +117,7 @@ class lizardfs::ha::pacemaker(
         'promote' => { 'interval' => '0', 'timeout' => '1800s' },
         'demote'  => { 'interval' => '0', 'timeout' => '1800s' },
       },
-    }~> Cs_commit['puppet']
+    } ~> Cs_commit['puppet']
 
     cs_primitive { 'Failover-IP':
       primitive_class => 'ocf',
@@ -135,18 +136,19 @@ class lizardfs::ha::pacemaker(
     cs_colocation { 'ip_with_master':
       primitives => [ ['Failover-IP', 'ms_lizardfs-master:Master'], ],
       cib        => 'puppet'
-    }~> Cs_commit['puppet']
+    } ~> Cs_commit['puppet']
 
     cs_order { 'master-after-ip':
       first   => 'Failover-IP:start',
       second  => 'ms_lizardfs-master:promote',
       require => Cs_colocation['ip_with_master'],
       cib     => 'puppet'
-    }~> Cs_commit['puppet']
+    } ~> Cs_commit['puppet']
 
     cs_shadow {
       'puppet':
     }
+
     cs_commit {
       'puppet':
     }
@@ -164,7 +166,6 @@ class lizardfs::ha::pacemaker(
       content => template("lizardfs/lizardfs.ocf"),
       before  => Service['pacemaker'],
     }
-
 }
 
 # vim:et:sw=2:ts=2:sts=2:tw=0:fenc=utf-8
