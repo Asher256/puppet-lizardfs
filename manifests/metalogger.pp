196814ca0a9053988ef7db38ea64662dd5eb4e74
Asher256 <Asher256@users.noreply.github.com>
2017-01-24 14:43:59 -0500

    metalogger secure_dir_permission

--- manifests/metalogger.pp
+++ manifests/metalogger.pp
@@ -85,7 +85,7 @@ class lizardfs::metalogger(
 
   if $create_data_path {
     exec { "metalogger: create ${data_path}":
-      command => "install -o '${::lizardfs::user}' -g '${::lizardfs::group}' -d '${data_path}'",
+      command => "install -o '${::lizardfs::user}' -g '${::lizardfs::group}' -d '${data_path}' -m '${::lizardfs::secure_dir_permission}'",
       creates => $data_path,
       unless  => "test -e '${data_path}'",
       before  => File["${::lizardfs::cfgdir}/mfsmetalogger.cfg"],
