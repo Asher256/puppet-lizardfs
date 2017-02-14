b6b5c635694b86d2213da7db6460d072aff57ddb
Asher256 <Asher256@users.noreply.github.com>
2017-02-14 17:07:21 -0500

    lfs_backup_metadata script

--- templates/etc/lizardfs/lfs_backup_metadata.sh
+++ templates/etc/lizardfs/lfs_backup_metadata.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 #
-# Script to help you backup your LizardFS Metadata
-# (hourly backups + daily backups).
+# This script will help you backup some LizardFS files.
+# It can tar + gz a list of files to a .tar.gz destination.
+#
+# Features:
+#    1. The script waits until the source files aren't open by a process
+#    (because if you want, for example, to backup metadata.mfs, it is better to
+#    wait until it is closed).
+#    2. It uses pigz to compress the backuped files (very fast on a multi-cpu
+#    server).
 #
 # Author: Achraf Cherti (aka Asher256) <asher256@gmail.com>
 #
@@ -11,12 +18,36 @@
 # Example
 # -------
 # Hourly backup (for the last 24h):
-#     lfs_backup_metadata "/var/lib/lizardfs/metadata.mfs.1" "/opt/backup/lizardfs-metadata/metadata.mfs-hour-$(date +%0H).gz"
+#     lfs_backup_metadata "/var/lib/lizardfs/metadata.mfs.1" "/opt/backup/lizardfs-metadata/metadata.mfs-hour-$(date +%0H).tar.gz"
 #
 
 set -o errexit
 set -o nounset
 # set -o xtrace
+
+#
+# TRACEBACK
+#
+function bash_traceback() {
+  local lasterr="$?"
+  set +o xtrace
+  local code="-1"
+  local bash_command=${BASH_COMMAND}
+  echo "Error in ${BASH_SOURCE[1]}:${BASH_LINENO[0]} ('$bash_command' exited with status $lasterr)" >&2
+  if [ ${#FUNCNAME[@]} -gt 2 ]; then
+    # Print out the stack trace described by $function_stack
+    echo "Traceback of ${BASH_SOURCE[1]} (most recent call last):" >&2
+    for ((i=0; i < ${#FUNCNAME[@]} - 1; i++)); do
+    local funcname="${FUNCNAME[$i]}"
+    [ "$i" -eq "0" ] && funcname=$bash_command
+    echo -e "  ${BASH_SOURCE[$i+1]}:${BASH_LINENO[$i]}\t$funcname" >&2
+    done
+  fi
+  echo "Exiting with status ${code}" >&2
+  exit "${code}"
+}
+trap 'bash_traceback' ERR
+set -o errtrace
 
 #
 # CHECK_ERRORS
@@ -27,12 +58,31 @@ check_errors() {
     exit 1
   fi
 
-  if ! [  -f "$LFS_METADATA_SOURCE" ]; then
-    echo "ERROR: the file '$LFS_METADATA_SOURCE' does not exist." >&2
+  local path
+
+  for path in "${BACKUP_SOURCE_FILES[@]}"; do
+    if ! [[ -e "$path" ]]; then
+      echo "ERROR: the path '$path' does not exist." >&2
+      exit 1
+    fi
+  done
+
+  path=''
+  path=$(readlink -e "$BACKUP_DEST_FILE" || true)
+  path=$(dirname "$path" || true)
+  if [[ "$path" == "" ]] || ! [[ -d "$path" ]]; then
+    echo "ERROR: the directory '$BACKUP_DEST_FILE' does not exist." >&2
+    exit 1
+  fi
+
+  if ! [[ $BACKUP_DEST_FILE =~ \.tar\.gz$ ]]; then
+    echo "ERROR: the extension of '$BACKUP_DEST_FILE' needs to be '.tar.gz'." >&2
     exit 1
   fi
 
   local cmd
+  # pigz: for the compression (multi-cpu)
+  # lsof: for the function wait_until_closed
   for cmd in pigz lsof; do
     if ! which "$cmd" >/dev/null 2>&1; then
       echo "You need to install '$cmd' with: apt-get install $cmd" >&2
@@ -75,14 +125,13 @@ wait_until_closed() {
 #
 # ATEXIT
 #
-SUCCESS=0
 atexit() {
   local errno="$?"
 
-  # Only of LFS_METADATA_DEST is declared
-  if [[ "${LFS_METADATA_DEST:-}" != '' ]]; then
-    if [[ $SUCCESS -ne 1 ]] && [[ -f $LFS_METADATA_DEST ]]; then
-      echo "[DELETING] $LFS_METADATA_DEST"
+  # Only of BACKUP_DEST_FILE is declared
+  if [[ "${BACKUP_DEST_FILE:-}" != '' ]]; then
+    if [[ $SUCCESS -ne 1 ]] && [[ -f $BACKUP_DEST_FILE ]]; then
+      echo "[DELETING] $BACKUP_DEST_FILE"
     fi
   fi
 
@@ -95,6 +144,8 @@ trap 'atexit' INT TERM EXIT QUIT
 # MAIN
 #
 main() {
+  SUCCESS=0
+
   if [[ $# -lt 2 ]]; then
     echo "Usage: $0 <LIZARDFS-METADATA-PATH> <PATH-TO-BACKUP.gz>" >&2
     echo "" >&2
@@ -102,23 +153,27 @@ main() {
     exit 1
   fi
 
-  # DYNAMIC VARIABLES
-  LFS_METADATA_SOURCE="$1"
-  LFS_METADATA_DEST="$2"
+  # The last argument is the destination
+  BACKUP_DEST_FILE=${*:${#@}}
+
+  # The source is all arguments except the last one
+  BACKUP_SOURCE_FILES=(${@:1:$(($#-1))})
 
   check_errors
+
   # wait until the source file is closed
-  wait_until_closed "$LFS_METADATA_SOURCE"
+  # wait_until_closed "$BACKUP_SOURCE_FILES"
 
   # backup the file
-  echo "[BACKUP] $LFS_METADATA_SOURCE --> $LFS_METADATA_DEST"
-  if pigz --force --stdout "$LFS_METADATA_SOURCE" > "$LFS_METADATA_DEST"; then
+  echo "[BACKUP] ${BACKUP_SOURCE_FILES[*]} --> ${BACKUP_DEST_FILE}"
+  #if tar -c --use-compression-program=pigz -f "$BACKUP_DEST_FILE" "$BACKUP_SOURCE_FILES"; then
+  if tar --verbose -c -f - "${BACKUP_SOURCE_FILES[@]}" | pigz --stdout > "$BACKUP_DEST_FILE"; then
     echo
-    echo "[BACKUP SUCCESSFUL] $LFS_METADATA_DEST"
+    echo "[BACKUP SUCCESSFUL] $BACKUP_DEST_FILE"
     SUCCESS=1
   else
     echo
-    echo "[BACKUP FAILED] $LFS_METADATA_DEST"
+    echo "[BACKUP FAILED] $BACKUP_DEST_FILE"
   fi
 
   exit 0
